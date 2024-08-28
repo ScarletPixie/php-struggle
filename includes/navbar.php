9ce9d05c4ca95378d8d137e6a76d91930a0dacc3
akino <scarletskynned@gmail.com>
2024-08-28 13:46:24 -0300

    added footer/navbar templates

---
+++ includes/navbar.php
@@ -0,0 +1,13 @@
+<nav id="navbar">
+	<ul>
+		<li>
+			<a href="index.php"> home </a>
+		</li>
+		<li>
+			<a href="contact.php"> contact </a>
+		</li>
+		<li>
+			<a href="about.php"> about </a>
+		</li>
+	</ul>
+</nav>
