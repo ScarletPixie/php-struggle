675b79ef54be4355c835c4c112cabad47f9949cc
akino <scarletskynned@gmail.com>
2024-08-28 11:04:11 -0300

    added basic structure

---
+++ public/about.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/general.css">
+	<title>Document</title>
+</head>
+<body>
+	<?php include '../include/navbar.php'; ?>
+
+	<main id="main">
+		<h1>TODO: about</h1>
+	</main>
+
+	<?php include '../include/footer.php'; ?>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/general.css">
+	<title>All Hail PHP</title>
+</head>
+<body>
+	<?php include('../includes/navbar.php') ?>
+
+	<main id="main">
+		<h1>Hello World</h1>
+	</main>
+
+	<?php include('../includes/footer.php') ?>
+</body>
+</html>
