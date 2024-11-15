1e5f1bf1a032a7be5d90708a9046806376032377
Charlotte <scarletskynned@gmail.com>
2024-11-15 17:03:16 -0300

    adding script to created database and user

--- website-0/config/setup_app_db.php
+++ website-0/config/setup_app_db.php
@@ -16,5 +16,11 @@ foreach ($dbCredentialsNames as $cred)
 	define($cred, $value);
 }
 
-$mysql = new mysqli(DB_HOST, DB_ADMIN_USER, DB_ADMIN_PASS
+$mysql = new mysqli(DB_HOST, DB_ADMIN_USER, DB_ADMIN_PASS);
+if ($mysql->connect_errno)
+{
+	echo "could not stablish connection to database: " . $mysql->error . PHP_EOL;
+	exit(2);
+}
 
+$query = $mysql->prepare('CREATE DATABASE IF NOT EXISTS ?');
