2738dd202b1589c9f036b5186a1e933949c1d535
Charlotte <scarletskynned@gmail.com>
2024-11-15 19:36:22 -0300

    adding script to created database and user

--- website-0/config/setup_app_db.php
+++ website-0/config/setup_app_db.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 $dbCredentialsNames = [
 	'DB_HOST', 'DB_NAME',
 	'DB_USER', 'DB_PASS',
@@ -16,6 +18,7 @@ foreach ($dbCredentialsNames as $cred)
 	define($cred, $value);
 }
 
+
 $mysql = new mysqli(DB_HOST, DB_ADMIN_USER, DB_ADMIN_PASS);
 if ($mysql->connect_errno)
 {
@@ -23,4 +26,60 @@ if ($mysql->connect_errno)
 	exit(2);
 }
 
-$query = $mysql->prepare('CREATE DATABASE IF NOT EXISTS ?');
+
+$db_name = DB_NAME;
+$db_host = DB_HOST;
+$db_user = DB_USER;
+$db_pass = DB_PASS;
+
+//	CREATE DATABASE
+$query = xquery_prep($mysql, "CREATE DATABASE IF NOT EXISTS `?`");
+$query->bind_param("s", $db_name);
+xquery_exec($mysql, $query);
+$query->close();
+
+
+//	CREATE TABLES
+//		create users table
+$mysql->select_db($db_name);
+$query = xquery_prep($mysql, "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL)");
+xquery_exec($mysql, $query);
+$query->close();
+//		create tasks table
+$query = xquery_prep($mysql, "CREATE TABLE IF NOT EXISTS tasks (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, description TEXT, user_id INT, FOREIGN KEY (user_id) REFERENCES users(id))");
+xquery_exec($mysql, $query);
+$query->close();
+
+
+//	CREATE DB USER
+//		create user
+$query = xquery_prep($mysql, "CREATE USER IF NOT EXISTS '?'@'?' IDENTIFIED BY '?'");
+$query->bind_param('sss', $db_user, $db_host, $db_pass);
+xquery_exec($mysql, $query);
+$query->close();
+//		give permissions on db
+$query = xquery_prep($mysql, "GRANT ALL PRIVILEGES ON `?`.* TO '?'@'?'");
+$query->bind_param('sss', $db_name, $db_user, $db_host);
+xquery_exec($mysql, $query);
+$query->close();
+//		flush permissions
+$query = xquery_prep($mysql, 'FLUSH PRIVILEGES');
+xquery_exec($mysql, $query);
+$query->close();
+
+
+$mysql->close();
+
+//	HELPER FUNCTIONS
+function xquery_exec(mysqli $conn, mysqli_stmt $stmt): void
+{
+	if (!$stmt->execute())
+		die("cannot execute query: ".$conn->error.PHP_EOL);
+}
+function xquery_prep(mysqli $conn, string $query): mysqli_stmt
+{
+	$stmt = $conn->prepare($query);
+	if ($stmt === false)
+		die("cannot prepare query: ".$conn->error.PHP_EOL);
+	return $stmt;
+}
